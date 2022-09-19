913c4e9ccc9553cf5433af44906d7f9f8c743173
devdavinci <dev.muhammad222000@gmail.com>
2022-09-19 20:21:58 +0200

    final api

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const path = require("path");
-const expressLayouts = require("express-ejs-layouts");
 const authorRoutes = require("./routes/authorRoutes");
 const bookRoutes = require("./routes/bookRoutes");
-const viewsRoutes = require("./routes/viewsRoutes");
 const app = express();
 
 app.use(express.json()); // to read json body
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.set("layout", "layouts/layout");
-app.use(expressLayouts);
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", viewsRoutes);
 app.use("/api/v1/authors", authorRoutes);
 app.use("/api/v1/books", bookRoutes);
 app.get("/", (req, res) => {
